29b1e0fa087c04a9660f67b98d64f940728c44a3
Cuong Phan <cuong_phan@datahouse.com>
2023-06-16 20:42:30 +0700

    update for testing prod-local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5001',
-  exploreurl: 'http://localhost:5072',
-  authorityUrl: 'http://localhost:5001',
+  apiUrl: 'https://localhost:5001',
+  exploreurl: 'https://localhost:5072',
+  authorityUrl: 'https://localhost:5001',
   notificationUrl: 'https://localhost:5044',
-  postUrl: 'http://localhost:5201',
+  postUrl: 'https://localhost:5201',
   clientId: 'together_we_go_angular_client',
-  clientUrl: 'http://localhost:4200',
+  clientUrl: 'https://localhost:4200'
 };

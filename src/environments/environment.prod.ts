aa5006566c900041e3187f1d1f07e97da52af798
Cuong Phan <cuong_phan@datahouse.com>
2023-07-03 00:56:46 +0700

    update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:5001',
-  exploreurl: 'https://localhost:5072',
-  authorityUrl: 'https://localhost:5001',
-  notificationUrl: 'https://localhost:5044',
-  postUrl: 'https://localhost:5201',
+  apiUrl: 'http://23.102.92.127:8081',
+  exploreurl: 'http://23.102.92.127:8085',
+  authorityUrl: 'http://23.102.92.127:8081',
+  notificationUrl: 'http://20.210.207.194:8087',
+  postUrl: 'http://20.210.207.194:8083',
   clientId: 'together_we_go_angular_client',
-  clientUrl: 'https://localhost:4200'
+  clientUrl: 'https://localhost:4200',
 };
